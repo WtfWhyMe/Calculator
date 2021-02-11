bf1a66b239cef9b26cd4176b803f185062ee670f
Sergey <g00354709@gmit.ie>
2021-02-11 14:45:00 +0000

    Second Commit

--- src/main/java/ie/gmit/Calculator.java
+++ src/main/java/ie/gmit/Calculator.java
@@ -3,4 +3,25 @@ package ie.gmit;
 public class Calculator {
     int firstNum;
     int secondNum;
+
+    Calculator(int f, int s){
+        this.firstNum = f;
+        this.secondNum = s;
+    }
+
+    public int getFirstNum() {
+        return firstNum;
+    }
+
+    public void setFirstNum(int firstNum) {
+        this.firstNum = firstNum;
+    }
+
+    public int getSecondNum() {
+        return secondNum;
+    }
+
+    public void setSecondNum(int secondNum) {
+        this.secondNum = secondNum;
+    }
 }
